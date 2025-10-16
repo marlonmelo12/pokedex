747e5b46101f480387de6f8a78a95f5e2dd23a32
Marlon <marlonmoura3000@gmail.com>
2025-10-15 21:44:09 -0300

    feat(backend): Creating urls

---
+++ backend/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UsuarioViewSet, TipoPokemonViewSet, PokemonUsuarioViewSet
+
+router = DefaultRouter()
+
+router.register(r'usuarios', UsuarioViewSet, basename='usuario')
+router.register(r'tipos', TipoPokemonViewSet, basename='tipopokemon') 
+router.register(r'pokemons', PokemonUsuarioViewSet, basename='pokemonusuario')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
